b5a46711f1f3dfb2a6cab7ad1aa4a05d1c401b55
Mike Egan <74567500+mikeegan400@users.noreply.github.com>
2022-11-21 07:45:08 -0600

    Add files via upload

---
+++ Test-Unique_lastname.ps1
@@ -0,0 +1,20 @@
+﻿PARAM(
+[parameter(mandatory=$true)]$FirstName
+,[parameter(mandatory=$true)]$LastName
+)
+
+import-module activedirectory
+
+[int]$counter = 2
+$tempLastName = $LastName
+
+$results = get-aduser -filter * -properties * | where-object {$_.givenName -eq $FirstName -and $_.sn -like $tempLastName} | select-object givenname,sn | Measure-object
+
+While ($results.count -eq 1)
+{
+	$tempLastName = $LastName + $counter
+	$results = get-aduser -filter * -properties * | where-object {$_.givenName -eq $FirstName -and $_.sn -like $tempLastName} | select-object givenName,sn | measure-object
+	$counter++
+}
+
+$tempLastName
---
+++ get-unique_first_last_name.ps1
@@ -0,0 +1,8 @@
+﻿PARAM(
+[parameter(mandatory=$true)]$FirstName
+,[parameter(mandatory=$true)]$LastName
+)
+
+$tempLastName = powershell -version 3.0 -command "e:\scripts\test_unique_lastname.ps1 $firstName $lastname"
+
+$tempLastName
