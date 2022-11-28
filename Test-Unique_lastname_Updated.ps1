51f3ffd1be49221c1677ef819aee5c39fa3bc246
Mike Egan <74567500+mikeegan400@users.noreply.github.com>
2022-11-28 09:08:53 -0600

    Add files via upload

---
+++ Test-Unique_lastname_Updated.ps1
@@ -0,0 +1,21 @@
+﻿PARAM(
+[parameter(mandatory=$true)]$FirstName
+,[parameter(mandatory=$true)]$LastName
+)
+
+import-module activedirectory
+
+[int]$counter = 2
+$tempLastName = $LastName
+
+$results = get-aduser -filter(givenName -eq $FirstName -and surname -like $tempLastName) | select-object givenName,surname | measure-object
+
+While ($results.count -eq 1)
+{
+	$tempLastName = $LastName + $counter
+	$results = get-aduser -filter(givenName -eq $FirstName -and surname -like $tempLastName) | select-object givenName,surname | measure-object
+
+  $counter++
+}
+
+$tempLastName
